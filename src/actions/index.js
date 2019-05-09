7fa7c13b6c4e28c9b00521c93680b36fa1c45f75
steve <jeaniedaddy@gmail.com>
2019-05-09 15:42:10 -0400

    176 List Building!

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,6 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 
 export const fetchPosts = ()=> async (dispatch)=>{
-        const posts = await jsonPlaceholder.get('/posts');
-        dispatch({ type: 'FETCH_POSTS', payload: posts});
+        const response = await jsonPlaceholder.get('/posts');
+        dispatch({ type: 'FETCH_POSTS', payload: response.data});
 };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware} from 'redux';
-import Thunk from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, applyMiddleware(Thunk));
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
