5d68d6851d8eaa23a0f9c1903a4f99afe6c9d244
steve <jeaniedaddy@gmail.com>
2019-05-09 08:33:19 -0400

    165 Shortened Syntax with Redux Thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,7 @@
-export const fetchPosts = () => {
-    return { type: 'FETCH_POSTS'};
-}
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+
+export const fetchPosts = ()=> async (dispatch)=>{
+        const posts = await jsonPlaceholder.get('/posts');
+        dispatch({ type: 'FETCH_POSTS', payload: posts});
+};
