71c96f77f6d4a1ac67095a78e4ffeb3f95aa81ef
steve <jeaniedaddy@gmail.com>
2019-05-10 13:26:43 -0400

    189. Quick Refactor with Chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,10 +4,14 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPostsAndUsers = () => async (dispatch, getState)=> {
     await dispatch(fetchPosts());
 
-    const userIds = _.uniq( _.map(getState().posts, 'userId'));
-    userIds.forEach(Id => {
-        dispatch(fetchUser(Id));
-    });
+    // const userIds = _.uniq( _.map(getState().posts, 'userId'));
+    // userIds.forEach(Id =>  dispatch(fetchUser(Id)));
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value()
 }
 export const fetchPosts = ()=> async (dispatch)=>{
         const response = await jsonPlaceholder.get('/posts');
