960c88e2d1d150de7c67974942eca56cfdf1d63a
steve <jeaniedaddy@gmail.com>
2019-05-10 13:17:12 -0400

    188. Finding Unique User ids

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,31 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState)=> {
+    await dispatch(fetchPosts());
 
-
+    const userIds = _.uniq( _.map(getState().posts, 'userId'));
+    userIds.forEach(Id => {
+        dispatch(fetchUser(Id));
+    });
+}
 export const fetchPosts = ()=> async (dispatch)=>{
         const response = await jsonPlaceholder.get('/posts');
-        dispatch({ type: 'FETCH_POSTS', payload: response.data});
+        dispatch ({ type: 'FETCH_POSTS', payload: response.data});
 };
 
-// export const fetchUser = (id)=> async (dispatch)=>{
-//     const response = await jsonPlaceholder.get(`/users/${id}`);
-//     //console.log(response.data);
-//     dispatch({ type: 'FETCH_USER', payload: response.data});
-// };
-
-const _fetchUser= _.memoize(async (id,dispatch) =>{
+export const fetchUser = (id)=> async (dispatch)=>{
     const response = await jsonPlaceholder.get(`/users/${id}`);
+    //console.log(response.data);
     dispatch({ type: 'FETCH_USER', payload: response.data});
-});
+};
 
-export const fetchUser = (id)=>  async (dispatch)=>_fetchUser(id,dispatch);
+ 
+// using lodash memoize function
+// const _fetchUser= _.memoize(async (id,dispatch) =>{
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: 'FETCH_USER', payload: response.data});
+// });
+
+// export const fetchUser = (id)=>  async (dispatch)=>_fetchUser(id,dispatch);
 
