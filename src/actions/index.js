8a21b6333ba45ce81c3381a391751487e4c2057e
steve <jeaniedaddy@gmail.com>
2019-05-10 11:35:50 -0400

    185. Alternate Overfetching Solution

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
 
 
 export const fetchPosts = ()=> async (dispatch)=>{
@@ -6,8 +8,16 @@ export const fetchPosts = ()=> async (dispatch)=>{
         dispatch({ type: 'FETCH_POSTS', payload: response.data});
 };
 
-export const fetchUser = (id)=> async (dispatch)=>{
+// export const fetchUser = (id)=> async (dispatch)=>{
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     //console.log(response.data);
+//     dispatch({ type: 'FETCH_USER', payload: response.data});
+// };
+
+const _fetchUser= _.memoize(async (id,dispatch) =>{
     const response = await jsonPlaceholder.get(`/users/${id}`);
-    //console.log(response.data);
     dispatch({ type: 'FETCH_USER', payload: response.data});
-};
+});
+
+export const fetchUser = (id)=>  async (dispatch)=>_fetchUser(id,dispatch);
+
